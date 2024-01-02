f728bab0605e0ea4f3616ed1ba8564af9f43c79b
CoMraDE <145497847+iahiaimon@users.noreply.github.com>
2024-01-02 17:38:16 +0600

    Delete PORTFOLIO directory

--- PORTFOLIO/portfolio.js
+++
@@ -1,7 +0,0 @@
-burger = document.querySelector('.burger');
-navList = document.querySelector('.nav-list');
-navbar = document.querySelector('.navbar');
-burger.addEventListener('click' , ()=>{
-    navList.classList.toggle('v-class');
-    navbar.classList.toggle('h-nav');
-})
